a60eace35818b79193f69f089e8116e752020e9d
MrPiThon <pythonexploitshtml@gmail.com>
2024-10-04 08:45:55 -0700

    Maybe fixed the project images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,19 +15,19 @@ const HomePage = () => {
                     title={"Zombie Siege"}
                     description={"A round-based Minecraft Zombie Survival Server chasing the feeling of OG CoD Zombies"}
                     url={"https://discord.gg/jBF5Jb7Cg9"}
-                    image="public/zombie-siege2.png"
+                    image="/zombie-siege2.png"
                 />
                 <ProjectCard
                     title={"A1 Pools"}
                     description={"A website for the A1 Pool Company in the US, not affiliated with the real A1 Pool Company"}
                     url={"https://sunny-stroopwafel-e64f2e.netlify.app/"}
-                    image="public/A1 Pool Hero Animated.gif"
+                    image="/A1 Pool Hero Animated.gif"
                 />
                 <ProjectCard
                     title={"Coursey.Codes"}
                     description={"This website! This was made from scratch with React, Vite, TailwindCSS and Canvas"}
                     url={"https://coursey.codes"}
-                    image="public/MrPiThon Hero Logo Animated.gif"
+                    image="/MrPiThon Hero Logo Animated.gif"
                 />
             </div>
 
